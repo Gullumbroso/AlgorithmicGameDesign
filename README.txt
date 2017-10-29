f74f57404a7b4db29d5df1506f56131808212a83
inbal Bruker <inbali.bruker@gmail.com>
2017-10-29 20:52:29 +0200

    added drunk and readme

---
+++ README.txt
@@ -0,0 +1,10 @@
+Gilad Lumbroso 204781694
+Inbal Bruker 206096927
+
+DrunkBrain:
+The drunk spaceship follows the nearest spaceship and tries to shot it. But due to the amount of alcohol in it blood it aim with a random mistake. Also, the shield is raised randomly.
+
+ShieldAndShootBrain:
+As implied by its name, this spaceship constantly check the area for threats. It raises the shield if danger is near by. Its second priority is to follow close ships and shot them if possible.
+
+
---
+++ ex1/StarWars/Assets/Prefabs/Ships/DrunkBrain.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using StarWars.Actions;
+using Infra.Utils;
+
+namespace StarWars.Brains {
+	public class DrunkBrain : SpaceshipBrain {
+		public override string DefaultName {
+			get {
+				return "drunk";
+			}
+		}
+		public override Color PrimaryColor {
+			get {
+				return new Color((float)0xF9 / 0xFF, (float)0x6C / 0xFF, (float)0xC6 / 0xFF, 1f);
+			}
+		}
+		public override SpaceshipBody.Type BodyType {
+			get {
+				return SpaceshipBody.Type.TieFighter;
+			}
+		}
+		private Spaceship target = null;
+		private int shieldTimer = 3;
+		public override Action NextAction() {
+			
+			// Find nearest target
+			var distance = float.MaxValue;
+			foreach (var ship in Space.Spaceships) {
+				// Make sure not to target self or dead spaceships and choose the closest one.
+				if (spaceship != ship && ship.IsAlive) {
+					if (spaceship.ClosestRelativePosition (ship).magnitude < distance) {
+						target = ship;
+						distance = spaceship.ClosestRelativePosition (ship).magnitude;
+					}
+				}
+			}
+			float mistake = Random.Range (-20f, 20f);
+			if (target != null) {
+				// Try to kill the target, but adding a little mistake to the aim.
+				var pos = spaceship.ClosestRelativePosition(target);
+				var forwardVector = spaceship.Forward;
+				var angle = pos.GetAngle(forwardVector) + mistake;
+				if (angle >= 10f) return TurnLeft.action;
+				if (angle <= -10f) return TurnRight.action;
+				if (distance < 15f && (!target.IsShieldUp)) { 
+					return spaceship.CanShoot ? Shoot.action : DoNothing.action;
+				}
+
+			}
+			/// Randomly put shield up and down for 3 turns
+			if (spaceship.IsShieldUp) {
+				shieldTimer--;
+				if (shieldTimer == 0) {
+					shieldTimer = 3;
+					return ShieldDown.action;
+
+				}
+			}
+			else if (spaceship.CanRaiseShield) {
+				int shield = Random.Range(0,10);
+				if(shield < 5){
+					return ShieldUp.action;
+				}
+
+			}
+			return DoNothing.action;
+		}
+	}
+}
+
